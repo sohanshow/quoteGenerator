ab501f56df3d76525f6a5e568676e4fadf743783
sohanshow <sohanshow@gmail.com>
2022-06-23 19:06:00 -0400

    + created a random quote from the list of the arrays in the API get

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let apiQuotes = [];
 
 
+
+
+//Showing new quotes
+
+function newQuote(){
+
+//Picking a random quote from apiQuotes array
+
+const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )]
+console.log(quote);
+
+}
+
 //Getting Quotes from API
 
 async function getQuotes(){
@@ -10,9 +23,8 @@ async function getQuotes(){
 
     const response = await fetch(apiURL);
     apiQuotes = await response.json();
-
-     console.log(apiQuotes);
-  }catch(erro){
+       newQuote();
+  }catch(error){
 
     //This is where I would catch my error
 
