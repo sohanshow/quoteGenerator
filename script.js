64dbc8dc5f439d3e5a9d0a29e5c14c165e4403f3
sohanshow <sohanshow@gmail.com>
2022-06-24 16:27:45 -0400

    + minor style changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const quoteContianer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
-
-
 
 
 //Showing new quotes
@@ -9,8 +14,10 @@ function newQuote(){
 
 //Picking a random quote from apiQuotes array
 
-const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )]
-console.log(quote);
+const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
+
+authorText.textContent = quote.author;
+quoteText.textContent = quote.text;
 
 }
 
