bbaee18d8e3ed6190271a4d1c2fcf9439ae3ea38
sohanshow <sohanshow@gmail.com>
2022-06-27 17:13:18 -0400

    + Script ready for pulling data from API and showing them in DOMs

--- script.js
+++ script.js
@@ -1,36 +1,77 @@
-const quoteContianer = document.getElementById('quote-container');
+let apiQuotes = [];
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const newQuoteBtn= document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
-let apiQuotes = [];
+//Show that we are loading
 
-
-//Showing new quotes
-
-function newQuote(){
-
-//Picking a random quote from apiQuotes array
-
-const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
-
-authorText.textContent = quote.author;
-quoteText.textContent = quote.text;
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 
 }
+
+
+
+//Hide loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+
+
+
+
+async function newQuote(){
+   loading();
+  
+  await sleep(500);
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  //Checking if author field if blank
+  if (!quote.author)
+   authorText.textContent = 'Unknown';
+   else
+   authorText.textContent = quote.author;
+
+//Checking the quote length for determining the styling
+if (quote.text.length > 120)
+ quoteText.classList.add('long-quote')
+else
+quoteText.classList.remove('long-quote')
+
+//Setting quote and hiding loader
+  quoteText.textContent = quote.text;
+  complete();
+
+
+}
+
+
+
 
 //Getting Quotes from API
 
 async function getQuotes(){
-
+  loading();
   const apiURL = 'https://type.fit/api/quotes';
   try{
 
     const response = await fetch(apiURL);
     apiQuotes = await response.json();
-       newQuote();
+   newQuote();
   }catch(error){
 
     //This is where I would catch my error
@@ -39,6 +80,20 @@ async function getQuotes(){
 }
 
 
-//Upon loading
+// Tweet Quote
+function tweetQuote() {
 
+ const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+ window.open(twitterUrl, '_blank');
+
+}
+
+
+//Event Listener
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//Upon loading
 getQuotes();
